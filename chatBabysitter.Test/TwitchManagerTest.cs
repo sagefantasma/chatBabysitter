13c478c712f149bc06569bac0ebb63c5c55e2e7b
Andrew Naro <a.naro1995@gmail.com>
2024-07-28 14:47:45 -0400

    starting the basic structure, starting with twitch integration

---
+++ chatBabysitter.Test/TwitchManagerTest.cs
@@ -0,0 +1,25 @@
+namespace chatBabysitter.Test
+{
+    public class TwitchManagerTest
+    {
+        [Fact]
+        public void CanReadChatMessage()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [Fact]
+        public void CanWriteChatMessage()
+        {
+            //Arrange
+            
+            //Act
+
+            //Assert
+        }
+    }
+}
---
+++ chatBabysitter/TwitchManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchLib.Client;
+using TwitchLib.Client.Models;
+
+namespace chatBabysitter
+{
+    public class TwitchManager(string activationString = "!")
+    {
+        internal JoinedChannel? JoinedChannel { get; set; } = null;
+        public string ActivationString { get; set; } = activationString;
+
+        private readonly TwitchClient _twitchClient = new TwitchClient();
+        private readonly string _username = "chatBabysitter";
+        private readonly string _twitchOAuth = "";
+
+        public bool ConnectToTwitch()
+        {
+            ConnectionCredentials connectionCredentials = new ConnectionCredentials(_username, _twitchOAuth);
+            _twitchClient.SetConnectionCredentials(connectionCredentials);
+
+            _twitchClient.OnMessageReceived += TwitchClient_OnMessageReceived;
+
+            return _twitchClient.Connect();
+        }
+
+        private void TwitchClient_OnMessageReceived(object? sender, TwitchLib.Client.Events.OnMessageReceivedArgs e)
+        {
+            ReadMessage(e.ChatMessage);
+        }
+
+        public void ReadMessage(ChatMessage receivedMessage)
+        {
+            string message = receivedMessage.Message;
+
+            if (message.StartsWith(ActivationString))
+            {
+                //TODO: stuff.
+            }
+        }
+
+        public void WriteMessage(string messageToSend)
+        {
+            if(JoinedChannel == null)
+            {
+                throw new InvalidOperationException("Not connected to a chat, can't write any messages Sadge");
+            }
+
+            _twitchClient.SendMessage(JoinedChannel, messageToSend);
+        }
+    }
+}
